c5d256f10c1963628dd78a3e380b6f2e5e481ba1
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-10-06 13:59:08 +0200

    modif du modele

--- src/convenet.py
+++ src/convenet.py
@@ -2,20 +2,23 @@ import keras
 import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from keras.layers import Dense, Flatten, Dropout
-from keras import Input, Model
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks.callbacks import EarlyStopping, Callback
 from keras.layers import add, Activation
 from keras.layers import Conv3D, MaxPool3D
-from keras.callbacks.callbacks import EarlyStopping, Callback
 from keras.models import Sequential
 from os import listdir
 
 PATH_TRAIN = "../data/x_train"
 PATH_TEST = "../data/x_test"
 
-def load_x(path):
+def load_x(path, control):
     X = [np.load("{}/{}".format(path,pocket))
-         for pocket in listdir(path)]
+         for pocket in listdir(path) 
+         if pocket not in control]
     X = [np.squeeze(array) for array in X]
     X = np.array(X)
     X = np.moveaxis(X, 1, -1)
@@ -31,7 +34,7 @@ def load_y(path, nucleotid, heme, steroid, control):
         elif pocket in steroid:
             Y.append(3)
         elif pocket in control:
-            Y.append(4)
+            continue 
     Y  = np.array(Y)
     return Y
 
@@ -52,8 +55,8 @@ nucleotid = list_generator("nucleotide.list.txt")
 heme = list_generator("heme.list.txt")
 steroid = list_generator("steroid.list.txt")
 control = list_generator("control.list.txt")
-X_train = load_x(PATH_TRAIN)
-X_test = load_x(PATH_TEST)
+X_train = load_x(PATH_TRAIN, control)
+X_test = load_x(PATH_TEST, control)
 Y_train = load_y(PATH_TRAIN, nucleotid, heme, steroid, control)
 Y_test = load_y(PATH_TEST, nucleotid, heme, steroid, control)
 one_hot_Y_train = one_hot_encoding(Y_train)
@@ -63,24 +66,27 @@ print(X_test.shape)
 print(one_hot_Y_train.shape)
 print(one_hot_Y_test.shape)
 
-model = Sequential()
-model.add(Conv3D(filters = 24, kernel_size = 12, strides=1, padding= "same", activation = "relu", kernel_initializer="he_normal"))
-#model.add(Conv3D(filters=64, kernel_size = 26, strides=1, padding= "same", activation = "relu", kernel_initializer="he_normal"))
-#model.add(Dropout(0.4))
-model.add(MaxPool3D(pool_size = 2, strides = 1, padding = "same"))
-model.add(Dropout(0.4))
-model.add(Flatten())
-model.add(Dense(units = 50, activation = "relu"))
-model.add(Dropout(0.4))
-model.add(Dense(units = 4, activation = "softmax"))
-
+def model_one():
+    model = Sequential()
+    model.add(Conv3D(filters = 21, kernel_size = 9, strides=1, padding= "same", activation = "relu", kernel_initializer="he_normal"))
+    #model.add(Conv3D(filters=64, kernel_size = 26, strides=1, padding= "same", activation = "relu", kernel_initializer="he_normal"))
+    model.add(Dropout(0.4))
+    model.add(MaxPool3D(pool_size = 2, strides = 1, padding = "same"))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(units = 50, activation = "relu"))
+    model.add(Dropout(0.4))
+    model.add(Dense(units = 3, activation = "softmax"))
+    model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=['accuracy'])
+    return model
+    
 cb = Callback()
-critor = EarlyStopping(monitor = "val_loss", patience = 5)
+critor = EarlyStopping(monitor = "val_loss", patience = 2)
 cb_list = [critor]
+my_model = model_one()
 
-model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=['accuracy'])
-model.fit(X_train[0:75,:,:,:,:],one_hot_Y_train[0:75,:],epochs=5, batch_size = 15,
-          validation_data = (X_test[0:35,:,:,:,:],one_hot_Y_test[0:35,:]), callbacks = cb_list)
-
-evaluation = model.evaluate(X_train, one_hot_Y_train)
+my_model.fit(X_train[0:75:,:,:,:],one_hot_Y_train[0:75,:],epochs=10, batch_size = 15,
+             validation_data = (X_test[0:25,:,:,:,:],one_hot_Y_test[0:25,:]), callbacks = cb_list)
+          
+evaluation = my_model.evaluate(X_train, one_hot_Y_train)
 print(evaluation)
